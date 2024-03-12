6b190f94b459ca869bf165509454c8676c3a62be
ThisisBillhe <56794062+ThisisBillhe@users.noreply.github.com>
2024-03-12 19:58:39 +0800

    Update collect_diffusion_input_imagenet.py
    remove setting CUDA_VISIBLE_DEVICES='7'

--- quant_scripts/collect_diffusion_input_imagenet.py
+++ quant_scripts/collect_diffusion_input_imagenet.py
@@ -3,7 +3,7 @@ import sys
 sys.path.append(".")
 sys.path.append('./taming-transformers')
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '7'
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 from taming.models import vqgan
 
 import torch
